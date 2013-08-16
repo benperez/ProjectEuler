37f7f31cd15630e3789e10b482ec897091a9c245
Stankalank <ben.prez@gmail.com>
2013-08-16 16:49:30 -0700

    reorganized and reformatted problems

--- p6.py
+++
@@ -1,8 +0,0 @@
-import time
-
-if __name__ == "__main__":
-	start = time.time()
-	n = 100
-	diff = 1.0/4.0*pow(n,4) + 1.0/6.0*pow(n,3) - 1.0/4.0*pow(n,2) - 1.0/6.0*n
-	elapsed = (time.time() - start)
-	print "Found %d in %s seconds" % (diff, elapsed)
