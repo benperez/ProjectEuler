29d9667ab880e9c3950e68a50a6250018cb97587
Benjamin Perez <ben.prez@gmail.com>
2014-02-12 14:30:28 -0800

    changed folder name

--- problems1_to_25/p020.py
+++
@@ -1,8 +0,0 @@
-from math import factorial
-import time
-
-if __name__ == "__main__":
-	start = time.time()
-	n = sum( (int(d) for d in str(factorial(100L)) ) )
-	elapsed = time.time() - start
-	print "found %d after %f seconds" % (n, elapsed)
